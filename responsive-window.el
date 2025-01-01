93d55d76f455e87a51aa1fba95fb7bbfd3f64adc
JenChieh <jcs090218@gmail.com>
2025-01-01 02:11:29 -0800

    Update copyright year

--- responsive-window.el
+++ responsive-window.el
@@ -1,6 +1,6 @@
 ;;; responsive-window.el --- Adapt to different screen sizes automatically  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2024  Shen, Jen-Chieh
+;; Copyright (C) 2024-2025  Shen, Jen-Chieh
 
 ;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
 ;; Maintainer: Shen, Jen-Chieh <jcs090218@gmail.com>
